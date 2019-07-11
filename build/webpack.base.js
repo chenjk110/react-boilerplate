cd94971b101f3e475b028a7c9a0bdcb71cf52eef
chenjk110 <chenjk110@qq.com>
2019-07-11 18:37:19 +0800

    add react, babel support

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -1,15 +1,34 @@
 const merge = require('webpack-merge')
-module.exports = merge({
-  entry: {
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
+const { commonConf } = require('./config')
+
+const { resove } = require('./utils')
+
+module.exports = merge({
+  mode: 'none',
+  context: __dirname,
+  entry: {
+    app: '../src/app.js'
+  },
+  output: {
+    filename: 'bundle.js',
+    path: resove('../dist'),
+    publicPath: '/'
   },
   module: {
     rules: [
-      {}
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
     ]
   },
   plugins: [
-    
+    new HtmlWebpackPlugin({
+      title: 'React Single Page Application',
+      template: resove('../src/index.html')
+    })
   ],
-
 })
